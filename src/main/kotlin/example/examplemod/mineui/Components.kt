1336aad46d01169fa2cfab24fd499892377781ed
SonMooSans <god63820869@gmail.com>
2022-09-18 16:39:15 +0800

    Improve Label and Stack Components

--- src/main/kotlin/example/examplemod/mineui/Components.kt
+++ src/main/kotlin/example/examplemod/mineui/Components.kt
@@ -1,19 +1,21 @@
 package example.examplemod.mineui
 
+import example.examplemod.mineui.drawer.Drawer
+import example.examplemod.mineui.drawer.Stack
 import java.awt.Color
 
-fun RenderContext.label(text: () -> String) = child(text::class) {
+fun RenderContext.label(text: () -> String) = label(text::class, text)
+
+fun RenderContext.label(key: Any, text: () -> String) = child(key) {
     drawer = Drawer {
         font.draw(stack, text(), 0F, 0F, Color.WHITE.rgb)
     }
 }
 
+fun RenderContext.stack(children: RenderContext.() -> Unit) = stack(children::class, children)
+
 fun RenderContext.stack(key: Any, children: RenderContext.() -> Unit) = child(key) {
-    drawer = Drawer {
-        for (child in rendered.children.values) {
-            child.draw(stack)
-        }
-    }
+    drawer = Stack()
 
     children(this) //render children
 }
