8b7ea8d53ffd930ee8b1ad3287629094141ef6e6
SonMooSans <god63820869@gmail.com>
2022-09-18 16:38:58 +0800

    Supports update and re-render

--- src/main/kotlin/example/examplemod/mineui/MineUI.kt
+++ src/main/kotlin/example/examplemod/mineui/MineUI.kt
@@ -1,35 +1,34 @@
 package example.examplemod.mineui
 
 import com.mojang.blaze3d.vertex.PoseStack
+import example.examplemod.mineui.drawer.DefaultDrawer
+import example.examplemod.mineui.drawer.Drawer
+import example.examplemod.mineui.hooks.Context
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.Font
 
-fun interface Drawer {
-    fun DrawContext.draw()
-}
-
-class DefaultDrawer: Drawer {
-    override fun DrawContext.draw() {
-        for (child in rendered.children.values) {
-            child.draw(stack)
-        }
-    }
-}
+@DslMarker
+annotation class DslBuilder
 
 fun component(render: RenderContext.() -> Unit): UI {
-    return UI(RenderContext().apply(render))
+    return UI(render)
 }
 
-class UI(
-    var context: RenderContext
-) {
+class UI(render: RenderContext.() -> Unit) {
+    val context = RenderContext(this, render)
+
+    init {
+        context.ui = this
+        context.render()
+    }
+
     fun draw(stack: PoseStack) {
         context.draw(stack)
     }
 }
 
 data class HookKey(
-    val type: String, val id: String
+    val type: String, val id: Any
 )
 
 class DrawContext(
@@ -39,28 +38,72 @@ class DrawContext(
     var font: Font = Minecraft.getInstance().font
 }
 
+@DslBuilder
 class RenderContext(
-    var drawer: Drawer = DefaultDrawer()
+    var ui: UI,
+    val render: RenderContext.() -> Unit,
+    var contexts: Map<Context<*>, Any?> = hashMapOf()
 ) {
-    val states = hashMapOf<String, Any?>()
-    val hooks = hashMapOf<HookKey, Any?>()
-    val children = hashMapOf<Any, RenderContext>()
+    val hooks: HashMap<HookKey, Any?> = hashMapOf()
+    var drawer: Drawer = DefaultDrawer()
 
-    fun child(key: Any, scope: RenderContext.() -> Unit) {
-        add(key, RenderContext().apply(scope))
+    val children = hashMapOf<Any, RenderContext>()
+    private var snapshot: Map<Any, RenderContext>? = null
+
+    fun update() {
+        render()
     }
 
+    fun render(): RenderContext {
+        //create snapshot
+        snapshot = HashMap(children)
+        children.clear()
+
+        //re-render children
+        render(this)
+
+        //clear snapshot
+        snapshot = null
+        return this
+    }
+
+    inline fun<reified T> use(key: HookKey): T? {
+        return hooks[key] as T?
+    }
+
+    inline fun<reified T> use(key: HookKey, initial: () -> T): T {
+        return hooks.getOrPut(key, initial) as T
+    }
+
+    fun<V> Context<V>.provider(
+        value: V,
+        render: RenderContext.() -> Unit
+    ) {
+        val prev = this@RenderContext.contexts
+        contexts = HashMap(prev).also {
+            it[this] = value
+        }
+
+        render(this@RenderContext)
+        contexts = prev
+    }
+
+    fun child(key: Any, render: RenderContext.() -> Unit) {
+        val context = snapshot?.get(key)?: RenderContext(ui, render, contexts)
+
+        add(key, context)
+    }
+
+    /**
+     * Add and Render given Context
+     */
     fun add(key: Any, element: RenderContext) {
-        children[key] = element
+        children[key] = element.render()
     }
 
     fun draw(stack: PoseStack) {
-        val drawer = this.drawer
-
-        if (drawer != null) {
-            with (drawer) {
-                DrawContext(stack, this@RenderContext).draw()
-            }
+        with (drawer) {
+            DrawContext(stack, this@RenderContext).draw()
         }
     }
 }
