c4849999e0086a3f3e631cd2a1f5633eb942d48b
SonMooSans <god63820869@gmail.com>
2022-09-18 16:39:53 +0800

    Add useMemo, useEffect, useState Hooks

---
+++ src/main/kotlin/example/examplemod/mineui/hooks/Effect.kt
@@ -0,0 +1,61 @@
+package example.examplemod.mineui.hooks
+
+import example.examplemod.mineui.HookKey
+import example.examplemod.mineui.RenderContext
+import kotlin.reflect.KProperty
+
+class MemoHook<V>(
+    val dependencies: Array<*>,
+    val value: V
+)
+
+fun<V> RenderContext.useMemo(vararg dependencies: Any?, call: () -> V): V {
+    val key = HookKey("useEffect", call::class)
+    val cache = use<MemoHook<V>>(key)
+
+    val updated = if (cache != null && cache.dependencies.contentEquals(dependencies)) {
+        cache
+    } else {
+        MemoHook(dependencies, call())
+    }
+    hooks[key] = updated
+    return updated.value
+}
+
+fun RenderContext.useEffect(vararg dependencies: Any?, call: () -> Unit) {
+    val key = HookKey("useEffect", call::class)
+    val cache = use<Array<Any?>>(key)
+
+    if (!cache.contentEquals(dependencies)) {
+        call()
+    }
+
+    hooks[key] = dependencies
+}
+
+inline fun<reified V> RenderContext.useState(noinline initial: () -> V) = useState(initial::class, initial())
+
+inline fun<reified V> RenderContext.useState(id: Any, initial: V): State<V> {
+    val key = HookKey("useState", id)
+    val value = use(key) { initial }
+
+    return State(value, this)
+}
+
+class State<V>(var value: V, val context: RenderContext) {
+    fun update(value: V) {
+        this.value = value
+        context.update()
+    }
+
+    operator fun component1() = value
+    operator fun component2() = ::update
+
+    operator fun<P> getValue(parent: P, property: KProperty<*>): V {
+        return value
+    }
+
+    operator fun<P> setValue(parent: P, property: KProperty<*>, value: V) {
+        update(value)
+    }
+}
