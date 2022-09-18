4e1734083ca13f52cec35f2076aa662760727d79
SonMooSans <god63820869@gmail.com>
2022-09-18 19:07:24 +0800

    Improve Example Screen

--- src/main/kotlin/example/examplemod/screen/ExampleScreen.kt
+++ src/main/kotlin/example/examplemod/screen/ExampleScreen.kt
@@ -1,21 +1,52 @@
 package example.examplemod.screen
 
 import com.mojang.blaze3d.vertex.PoseStack
+import example.examplemod.mineui.RenderContext
 import example.examplemod.mineui.component
+import example.examplemod.mineui.drawer.Stack
+import example.examplemod.mineui.hooks.Context
+import example.examplemod.mineui.hooks.createContext
+import example.examplemod.mineui.hooks.useContext
+import example.examplemod.mineui.hooks.useState
 import example.examplemod.mineui.label
+import example.examplemod.mineui.stack
 import net.minecraft.network.chat.Component
 
 import net.minecraft.client.gui.screens.Screen
+import java.awt.Color
+
+class Theme(
+    val text: String
+)
+
+val ThemeContext = createContext<Theme>()
 
 class ExampleGUI(p_96550_: Component?) : Screen(p_96550_) {
     val root = component {
-        label { "Hello World" }
+        ThemeContext.provider(Theme("Hello World")) {
+            test(0)
+        }
+
+        stack({padding(10); gap = 5; background = Color.BLACK}) {
+            label { "Hello World" }
+            label { "Hello Kane" }
+            label { "Hello MONEY" }
+        }
     }
 
-    override fun render(stack: PoseStack, a: Int, b: Int, c: Float) {
-        super.render(stack, a, b, c)
+    override fun mouseClicked(x: Double, y: Double, type: Int): Boolean {
+        return root.onClick(x, y, type)
+    }
 
-        root.draw(stack)
+    override fun render(stack: PoseStack, x: Int, y: Int, c: Float) {
+        super.render(stack, x, y, c)
+        root.draw(stack, x, y)
     }
 }
 
+fun RenderContext.test(key: Any) = child(key) {
+    val state by useState { "first" }
+    val theme = useContext(ThemeContext)
+
+    println("$key $state ${theme?.text}")
+}
