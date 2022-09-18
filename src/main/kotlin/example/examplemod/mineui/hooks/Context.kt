6b9e35de9a538ffcd3c5c106256fa0d4cc825ca8
SonMooSans <god63820869@gmail.com>
2022-09-18 16:40:10 +0800

    Add Context API and useContext Hook

---
+++ src/main/kotlin/example/examplemod/mineui/hooks/Context.kt
@@ -0,0 +1,17 @@
+package example.examplemod.mineui.hooks
+
+import example.examplemod.mineui.RenderContext
+
+class Context<V> internal constructor(val initial: V)
+
+inline fun<reified V> RenderContext.useContext(context: Context<V>): V {
+    return contexts.getOrDefault(context, context.initial) as V
+}
+
+fun<V> createContext(): Context<V?> {
+    return Context(null)
+}
+
+fun<V> createContext(initial: V): Context<V> {
+    return Context(initial)
+}
