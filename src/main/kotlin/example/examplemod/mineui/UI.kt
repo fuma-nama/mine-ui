8252917e33937fcf08eb4f1647719536b4b8533f
SonMooSans <god63820869@gmail.com>
2022-09-21 22:22:39 +0800

    Supports handling click events

--- src/main/kotlin/example/examplemod/mineui/UI.kt
+++ src/main/kotlin/example/examplemod/mineui/UI.kt
@@ -2,61 +2,66 @@ package example.examplemod.mineui
 
 import com.mojang.blaze3d.vertex.PoseStack
 import example.examplemod.mineui.core.Component
+import example.examplemod.mineui.element.Container
 import example.examplemod.mineui.element.StackLayout
+import example.examplemod.mineui.element.UIElement
 import example.examplemod.mineui.utils.Size
 import example.examplemod.mineui.wrapper.DrawStackDefault
 
 data class PosXY(
     val x: Int, val y: Int
-)
+) {
+    operator fun plus(other: PosXY): PosXY {
+        return PosXY(
+            other.x + this.x,
+            other.y + this.y
+        )
+    }
+}
 
 class UI(val size: Size? = null, render: Component.() -> Unit) {
-    val context = Component(this, render, element = StackLayout())
+    val root = Component(this, render, element = StackLayout())
 
     init {
-        context.ui = this
-        context.render()
+        root.ui = this
+        root.render()
     }
 
     fun draw(stack: PoseStack, mouseX: Int, mouseY: Int) {
-        context.draw(DrawStackDefault(stack), mouseX, mouseY, size)
+        root.draw(DrawStackDefault(stack), size)
     }
 
     fun onClick(x: Double, y: Double, type: Int): Boolean {
         println("$x, $y")
+        fireEvent(
+            this.root.element!!,
+            { element ->
+                val pos = element.absolutePosition
+                val size = element.absoluteSize
+
+                pos.x <= x && pos.x + size.width >= x &&
+                        pos.y <= y && pos.y + size.height >= y
+            },
+            { it.listener?.mouseClicked(x, y, type) == true }
+        )
         return true
     }
 
-    /*
-    fun findFocus(mouseX: Double, mouseY: Double): Component? {
-        fun isIn(data: LayoutData): Boolean {
-            val (pos, size) = data
-
-            return pos.x >= mouseX && pos.y <= mouseY &&
-                    mouseX <= size.width && mouseY <= size.height
+    fun fireEvent(
+        element: UIElement<*>,
+        filter: (UIElement<*>) -> Boolean,
+        action: (UIElement<*>) -> Boolean
+    ) {
+        if (filter(element) && action(element)) {
+            return
         }
 
-        fun find(data: LayoutData): Component? {
-            return if (isIn(data)) {
-                for (child in data.comp.element.layout) {
-                    val found = find(child)
-
-                    if (found != null) {
-                        return found
-                    }
-                }
-
-                data.comp
-            } else {
-                null
+        if (element is Container<*>) {
+            for (child in element.children) {
+                fireEvent(child, filter, action)
             }
         }
-
-        return find(LayoutData(
-            PosXY(0, 0), context.element.getSize(), context
-        ))
     }
-     */
 
     fun onHover(x: Double, y: Double, type: Int): Boolean = false
 }
