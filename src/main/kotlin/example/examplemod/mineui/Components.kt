1487f73006bb7c0ba9e2b8b7ee061cc9d5b26bf6
SonMooSans <god63820869@gmail.com>
2022-09-22 21:14:35 +0800

    Rewrite Button Component usage

--- src/main/kotlin/example/examplemod/mineui/Components.kt
+++ src/main/kotlin/example/examplemod/mineui/Components.kt
@@ -3,15 +3,15 @@ package example.examplemod.mineui
 import example.examplemod.mineui.core.Component
 import example.examplemod.mineui.element.*
 
-fun Component.label(style: LabelStyle.() -> Unit = {}, text: () -> String) = label(style::class, style, text)
+fun Component.label(style: LabelStyle.() -> Unit = {}, text: () -> String) = label(text::class, style, text)
 
 fun Component.label(key: Any, style: LabelStyle.() -> Unit = {}, text: () -> String) = child(key) {
     element(::TextElement, style).text = text()
 }
 
-fun Component.button(style: ButtonStyle.() -> Unit = {}, content: () -> String) = button(content::class, content(), style)
+fun Component.button(style: ButtonStyle.() -> Unit = {}, content: () -> String) = button(content::class, style, content())
 
-fun Component.button(key: Any, content: String, style: ButtonStyle.() -> Unit) = child(key) {
+fun Component.button(key: Any, style: ButtonStyle.() -> Unit = {}, content: String) = child(key) {
     element(::ButtonElement, style).setText(content)
 }
 
