7112f52b84aeae6d01f2531e12b1825182c57842
SonMooSans <god63820869@gmail.com>
2022-09-30 23:43:26 +0800

    Add Point4F

--- src/main/kotlin/example/examplemod/mineui/style/Point4.kt
+++ src/main/kotlin/example/examplemod/mineui/style/Point4.kt
@@ -1,5 +1,7 @@
 package example.examplemod.mineui.style
 
+import java.awt.Color
+
 data class Point4(
     val top: Int,
     val left: Int,
@@ -8,4 +10,27 @@ data class Point4(
 ) {
     val px: Int get() = left + right
     val py: Int get() = top + bottom
+
+    companion object {
+        val Empty = Point4(0, 0, 0, 0)
+    }
 }
+
+data class Point4F(
+    val top: Float,
+    val left: Float,
+    val right: Float,
+    val bottom: Float
+) {
+    val x: Float get() = left + right
+    val y: Float get() = top + bottom
+
+    companion object {
+        val Empty = Point4F(0F, 0F, 0F, 0F)
+    }
+}
+
+data class Border(
+    val size: Point4F,
+    val color: Color
+)
