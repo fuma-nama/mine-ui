2ba04a75f2b30f4775a17261b31fd881a72b5094
SonMooSans <god63820869@gmail.com>
2022-09-30 23:42:40 +0800

    Rewrite UI Event Manager

--- src/main/kotlin/example/examplemod/mineui/core/UI.kt
+++ src/main/kotlin/example/examplemod/mineui/core/UI.kt
@@ -7,15 +7,21 @@ import example.examplemod.mineui.element.layout.StackLayout
 import example.examplemod.mineui.style.PosXY
 import example.examplemod.mineui.utils.Size
 import example.examplemod.mineui.wrapper.DrawStackDefault
+import example.examplemod.mineui.wrapper.GUIListener
 import net.minecraft.client.gui.Gui
 
 class UI(var size: Size? = null, render: Component.() -> Unit) {
+    val state = UIState()
     val root = Component(this, render, element = StackLayout())
 
     init {
         root.ui = this
         root.render()
         reflow()
+    }
+
+    fun focus(element: UIElement<*>) {
+        this.state.focus = element
     }
 
     fun updateSize(w: Int, h: Int) {
@@ -38,10 +44,40 @@ class UI(var size: Size? = null, render: Component.() -> Unit) {
 
     fun onClick(x: Double, y: Double, type: Int): Boolean {
         fireEvent(
-            this.root.element!!,
+            EventType.Click,
             { element -> element.isIn(x, y) },
             { element, context ->
-                element.listener?.onClick(x, y, type, context)
+                element.onClick(x, y, type, context)
+            }
+        )
+        return true
+    }
+
+    fun onType(char: Char, key: Int): Boolean {
+        state.focus?.let {
+            it.onType(char, key, GuiEventContext(this, it))
+        }
+
+        return true
+    }
+
+    fun onMouseReleased(mouseX: Double, mouseY: Double, mouseButton: Int): Boolean {
+        fireEvent(
+            EventType.MouseRelease,
+            { element -> element.isIn(mouseX, mouseY) },
+            { element, context ->
+                element.onMouseReleased(mouseX, mouseY, mouseButton, context)
+            }
+        )
+        return true
+    }
+
+    fun onScroll(mouseX: Double, mouseY: Double, scroll: Double): Boolean {
+        fireEvent(
+            EventType.MouseScroll,
+            { element -> element.isIn(mouseX, mouseY) },
+            { element, context ->
+                element.onScroll(mouseX, mouseY, scroll, context)
             }
         )
         return true
@@ -55,38 +91,73 @@ class UI(var size: Size? = null, render: Component.() -> Unit) {
         dragY: Double
     ): Boolean {
 
-        fireEvent(this.root.element!!,
+        fireEvent(
+            EventType.MouseDrag,
             { element -> element.isIn(mouseX, mouseY) },
             { element, context ->
-                element.listener?.onDrag(mouseX, mouseY, mouseButton, dragX, dragY, context)
+                element.onDrag(mouseX, mouseY, mouseButton, dragX, dragY, context)
             }
         )
         return true
     }
 
     fun fireEvent(
-        element: UIElement<*>,
+        type: EventType,
         filter: (UIElement<*>) -> Boolean,
-        action: (UIElement<*>, GuiEventContext) -> Unit
+        action: (GUIListener, GuiEventContext) -> Unit
     ) {
-        if (filter(element)) {
+        var reflow = false
+
+        fun execute(element: UIElement<*>): GuiEventContext {
             val context = GuiEventContext(this, element)
             action(element, context)
 
-            if (context.reflow) reflow()
-            if (context.prevent) return
-        }
-
-        if (element is Container<*>) {
-            for (child in element.children.toList()) {
-                fireEvent(child, filter, action)
+            if (!reflow) {
+                reflow = context.reflow
             }
+            return context
         }
-    }
 
-    fun onHover(x: Double, y: Double, type: Int): Boolean = false
+        /**
+         * Returns True if event is prevented
+         */
+        fun next(element: UIElement<*>): Boolean {
+            if (element is Container<*>) {
+                for (child in element.children.toList()) {
+                    if (next(child)) return true
+                }
+            }
+
+            if (filter(element)) {
+                val executed = execute(element)
+
+                if (executed.prevent) return true
+            }
+
+            return false
+        }
+
+        val hooked = state.hooked[type]
+
+        if (hooked != null) {
+            execute(hooked)
+        } else {
+            next(this.root.element!!)
+        }
+
+        if (reflow) reflow()
+    }
 }
 
 data class HookKey(
     val type: String, val id: Any
 )
+
+enum class EventType {
+    Click, MouseMove, MouseScroll, MouseDrag, MouseRelease, KeyPressed, KeyReleased, Type
+}
+
+class UIState {
+    var focus: UIElement<*>? = null
+    val hooked = hashMapOf<EventType, UIElement<*>>()
+}
