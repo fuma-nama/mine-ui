f156f7a0cc17e927af701082b1b95be8c57c6714
SonMooSans <god63820869@gmail.com>
2022-10-04 12:35:25 +0800

    Improve GuiEventContext usage

--- src/main/kotlin/example/examplemod/mineui/core/GuiEventContext.kt
+++ src/main/kotlin/example/examplemod/mineui/core/GuiEventContext.kt
@@ -7,5 +7,15 @@ class GuiEventContext(val element: UIElement<*>) {
      * If the event is prevented, listener of children components will be skipped
      */
     var prevent: Boolean = false
+        private set
     var reflow: Boolean = false
+        private set
+
+    fun prevent() {
+        prevent = true
+    }
+
+    fun requireReflow() {
+        reflow = true
+    }
 }
